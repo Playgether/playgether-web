fd1560a5e49cf983cfb94ed71c8f317ffb2ac6a4
guilhermeveigaa <guilhermeveigafff@gmail.com>
2024-09-06 15:53:36 -0300

    feat:  Add profileLolContext and getProfileLol

---
+++ frontend/src/services/getProfileLol.ts
@@ -0,0 +1,27 @@
+import { api } from "./api";
+import { TokenData } from "./updateTokenRequest";
+
+export interface ProfileLolProps {
+    username: string;
+    rank: string;
+    tag: string;
+    id: number; 
+    account_id: string;
+    puuid: string;
+    summonerId: string;
+}
+
+export const getProfileLol = async (authTokens: TokenData | null | undefined, id_profile: number | undefined) => {
+    try {
+        const response = await api.get<ProfileLolProps[]>(`/api/v1/profiles/${id_profile}/infos/lol`, {
+            headers: {
+                'Authorization':'Bearer ' + String(authTokens?.access)
+            }
+        })
+        return response;   
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+
+};
