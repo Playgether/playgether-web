685511305638b9a44f79f349145ace8413949371
Vitor Daniel <vitordanieldeveloper@gmail.com>
2024-09-10 17:46:27 -0300

    First documentation

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
   },
   images: {
     domains: ['localhost', '127.0.0.1', '192.168.18.5', 'images.unsplash', 'images.unsplash.com'], 
+  },
+  env: {
+    baseUrl: process.env.LOCAL_API_URL
   }
+  
 }
 
 module.exports = nextConfig
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
+    baseURL: process.env.baseUrl,
     // baseURL: "http://127.0.0.1:8000",
-    baseURL: "http://127.0.0.1:8000",
     headers: {
         'Content-Type': 'application/json'
     }
