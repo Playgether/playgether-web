d95ea56f057c19face3fcb36fe782ea8d086cfdc
Vitor Daniel <vitordanieldeveloper@gmail.com>
2024-10-25 17:12:57 -0300

    fix: Making the right part of the feed have a minimium width

--- frontend/src/components/pages/feed/DesktopFeed/Right/Right.tsx
+++ frontend/src/components/pages/feed/DesktopFeed/Right/Right.tsx
@@ -6,7 +6,7 @@ import TopicsOfMoment from "./TopicsOfTheMomment/TopicsOfMoment"
 /** Este é o componente principal da parte "Right" da página de feed. Seu intuito é servir como wrapper de toda a parte direita da página. */
 const Right = () => {
     return (
-        <div className="w-full 2xl:sticky top-0 h-full 2xl:h-fit">
+        <div className="w-full 2xl:sticky top-0 h-full 2xl:h-fit min-w-[420px]">
             <div className="bg-white-200 flex-1 h-full flex flex-col space-y-4 sticky-last">
                 <div className="bg-white-200 h-3/6 flex mt-2 flex-col items-center space-y-2 shadow-lg rounded-lg w-10/12 2xl:w-[374px] min-h-[300px] 2xl:h-[400px] 2xl:max-h-[400px] max-h-[350px] 2xl:static">
                     <TopCard title={"Notificações recentes"}/>
