3658fdab0e12ae7ba87d401b5b2a41b53f319352
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2024-11-12 21:57:52 -0300

    ✨ feat: add GamesList

---
+++ frontend/src/types/games.ts
@@ -0,0 +1,18 @@
+export type gamesTypes = {
+    lol: {
+        component: React.JSX.Element;
+        src: string;
+        alt: string;
+    };
+    valorant: {
+        component: React.JSX.Element;
+        src: string;
+        alt: string;
+    };
+    cs2: {
+        component: React.JSX.Element;
+        src: string;
+        alt: string;
+    };
+
+}
