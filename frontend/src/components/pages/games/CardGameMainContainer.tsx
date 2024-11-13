6c564c18d4b3d03357dafe5fe692e282f3bedfd7
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2024-11-12 21:52:19 -0300

    ✨ feat: add new main component to cardGames

---
+++ frontend/src/components/pages/games/CardGameMainContainer.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+
+interface CardGameMainContainerProps {
+    children: React.ReactNode;
+}
+
+export const CardGameMainContainer: FC<CardGameMainContainerProps> = ({ children }) => {
+  return (
+
+    <>
+        <section className="text-center pt-20 font-extrabold text-orange-500 text-2xl">
+            <h1>Conecte seus jogos</h1>
+        </section>
+
+        {children}
+    </>
+
+  )
+}
