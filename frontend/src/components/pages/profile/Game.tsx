5388169e1869c435db63e444cb7d046bcf6af88c
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2024-11-12 21:55:29 -0300

    ♻️ refactor: refactor Games.tsx, add new logic to page

--- frontend/src/components/pages/profile/Game.tsx
+++ frontend/src/components/pages/profile/Game.tsx
@@ -1,41 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { CardGames } from "../games/CardGames"
-import CardImage from "../games/CardImage"
-import ProfileLol from "../games/ProfileLol/ProfileLol";
-import { ProfileLolContextProvider } from "../../../context/ProfileLolContext";
+import React, { useState } from "react";
+import { CardGameMainContainer } from "../games/CardGameMainContainer";
+import { CardGames } from "../games/CardGames";
+import CardImage from "../games/CardImage";
+import GamesList from "../games/gamesList/gamesList";
 
 export const Game = () => {
+
+    const [page, setPage] = useState<string | null>(null);
+
+    const handleBackPage = () => setPage(null);
+
     return (
-        <main className="">
-            <section className="text-center pt-20 font-extrabold text-orange-500 text-2xl">
-                <h1>Conecte seus jogos</h1>
-            </section>
-
-            <section className="flex gap-4 mt-20 items-center justify-center">
-                <CardGames>
-                    <CardImage src="/games/League of Legends.png" alt="LoL Banner" />
-                </CardGames>
-
-                <CardGames>
-                    <CardImage src="/games/Valorant.png" alt="Valorant Banner" />
-                </CardGames>
-
-                <CardGames>
-                    <CardImage src="/games/Counter Strike 2.png" alt="LoL Banner" />
-                </CardGames>
-
-            </section>
-
-            <section>
-                <h2 className="text-center mt-20 font-extrabold text-orange-500 text-2xl">
-                    Seus jogos conectados
-                </h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-black-400  pb-2">
-                    <ProfileLolContextProvider>
-                        <ProfileLol />             
-                    </ProfileLolContextProvider>
-                </div>
-            </section>
-        </main>
+        <>
+            {page  ? (
+                <>
+                    <button className="text-black-500" onClick={() => handleBackPage()}>
+                        voltar
+                    </button>
+                    {page && GamesList[page]?.component}
+                </>
+            ) : (
+                <CardGameMainContainer>
+                    <section className="flex gap-4 mt-20 items-center justify-center">
+                        {Object.keys(GamesList).map((game) => (
+                            <CardGames key={game} onClick={() => setPage(game)}>
+                                <CardImage src={GamesList[game].src} alt={GamesList[game].alt} />
+                            </CardGames>
+                        ))}
+                    </section>
+                </CardGameMainContainer>
+            )}
+        </>
     )
+
 }
