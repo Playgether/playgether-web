d60ec108c6fcf8d4715ed87efe88c5547c2976a2
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2024-11-12 21:53:02 -0300

    ✨ feat: add props interface and new props from CardGames

--- frontend/src/components/pages/games/CardGames.tsx
+++ frontend/src/components/pages/games/CardGames.tsx
@@ -1,9 +1,14 @@
 import React from "react"
 
+interface CardGamesProps {
+    children: React.ReactNode;
+    onClick: () => void;
+}
+
 /** Este é o Card padrão utilizado para rendenrizar as opção dos jogos */
-export const CardGames = ({ children }) => {
+export const CardGames: React.FC<CardGamesProps> = ({ children, onClick }) => {
     return (
-        <div className="max-w-[20rem] max-h-56 shadow-md shadow-slate-300 hover:cursor-pointer overflow-hidden">
+        <div className="max-w-[20rem] max-h-56 shadow-md shadow-slate-300 hover:cursor-pointer overflow-hidden" onClick={onClick}>
             {children}
         </div>
     )
