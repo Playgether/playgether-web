894c5b42503c85721c8865246721c6b7e51b05a2
Vitor Daniel <vitordanieldeveloper@gmail.com>
2025-02-10 14:10:25 -0300

    fix: Making profile request be on the server side and making profile page without params user the authenticated user username

--- src/components/pages/profile/Page.tsx
+++ src/components/pages/profile/Page.tsx
@@ -1,49 +1,23 @@
 "use client";
 import { useAuthContext } from "@/context/AuthContext";
-import { LeftProfile } from "./LeftProfile";
-import { RightProfile } from "./RightProfile";
-import { Suspense, useEffect, useState } from "react";
-import {
-  getProfileByUsername,
-  getProfileByUsernameProps,
-} from "@/services/getProfileByUsername";
-import NotFoundProfile from "@/app/profile/[username]/not-found";
 import { Props } from "@/app/profile/[username]/page";
-import { LoadingComponent } from "@/components/layouts/components/LoadingComponent";
+import ProfileBaseInformation from "./ProfileBaseInformations";
 
 const Page = ({ params }: Props) => {
   const { authTokens, user } = useAuthContext();
-  const [profile, setProfile] = useState<getProfileByUsernameProps | null>(
-    null
-  );
-
-  async function fetchProfile(username: string) {
-    const response = await getProfileByUsername(authTokens, username);
-    setProfile(response.data[0]);
-    return response;
-  }
-
-  useEffect(() => {
-    console.log(`TESTE ${user?.username}`);
-    if (params.username) {
-      fetchProfile(params.username);
-    } else {
-      fetchProfile(user?.username);
-    }
-  }, [params.username, user?.username]);
-
   return (
     <div className="max-w-[1420px] w-[90vw] flex flex-col items-center">
       <div className="mr-2 flex flex-col lg:flex-row md:flex-row mt-2 gap-2 w-full">
-        {profile ? (
-          <>
-            <LeftProfile profile={profile} />
-            <Suspense fallback={<LoadingComponent />}>
-              <RightProfile profile={profile} />
-            </Suspense>
-          </>
+        {params ? (
+          <ProfileBaseInformation
+            authTokens={authTokens}
+            username={params?.username}
+          />
         ) : (
-          <NotFoundProfile />
+          <ProfileBaseInformation
+            authTokens={authTokens}
+            username={user?.username}
+          />
         )}
       </div>
     </div>
