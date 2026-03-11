30599d1fd317dc310da2142f53bb9bbf67f40801
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2026-03-11 12:09:24 -0300

    fix: update baseURL to use NEXT_PUBLIC_API_URL environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.baseUrl,
-    // baseURL: "http://127.0.0.1:8000",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         'Content-Type': 'application/json'
     }
